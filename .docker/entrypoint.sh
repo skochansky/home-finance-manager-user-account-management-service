06dccd0e93de17a1d75b61bbe6ffc8d2e1b7b6b1
stanislaw <stanislaw.kochanski@protonmail.com>
2024-06-05 22:11:27 +0200

    refactor: get rid of django' ćx
    dd
    
    eqwe
    cćżćźźżćxxzXI~YZCVI~ZC

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -1,6 +1,1 @@
 #!/bin/bash
-
-poetry run python src/manage.py makemigrations
-poetry run python src/manage.py migrate
-
-poetry run python src/manage.py runserver 0.0.0.0:8100
--- src/accounts/urls.py
+++
@@ -1,5 +0,0 @@
-from django.urls import path
-
-from .views import test_endpoint
-
-urlpatterns = [path("test", test_endpoint, name="test-endpoint")]
